757b61aa5aeffe42ec768ce13554eec33d487bd7
Jake Atkin <jake.a.atkin@gmail.com>
2019-06-21 09:28:57 +0100

    Created comments to breakdown build MVClayout

--- todoMVC.Layout.js
+++ todoMVC.Layout.js
@@ -1,3 +1,19 @@
+//A layout is a specialized type of view that directly extends  Marionette.ItemView. This means it’s intended to render a single template and may or may not have a 
+//model (or item) associated with the template.
+
+//One of the main differences between a Layout and an ItemView is that the layout contains regions. When defining a Layout, we supply it with both a template and the 
+//regions that the template contains. After rendering the layout, we can display other views within the layout using the regions that were defined.
+
+//In our TodoMVC Layout module below, we define Layouts for:
+
+//Header: where we can create new Todos
+//Footer: where we summarize how many Todos are remaining/have been completed
+//This captures some of the view logic that was previously in our AppView and TodoView.
+
+//Note that Marionette modules (such as the below) offer a simple module system which is used to create privacy and encapsulation in Marionette apps. 
+//These certainly don’t have to be used however, and later on in this section we’ll provide links to alternative implementations using RequireJS + AMD instead.
+
+
 TodoMVC.module('Layout', function (Layout, App, Backbone, Marionette, $, _) {
 
     // Layout Header View
