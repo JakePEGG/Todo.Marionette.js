88cbd061862a6e418cefb869e2a92abf77d03618
Jake Atkin <jake.a.atkin@gmail.com>
2019-06-21 09:26:34 +0100

    Created comments to breakdown build

--- TodoMVC.js
+++ TodoMVC.js
@@ -9,3 +9,7 @@ TodoMVC.addRegions({
 TodoMVC.on('start', function () {
     Backbone.history.start();
 });
+
+// Regions are used to manage the content that’s displayed within specific elements, and the  addRegions method on the MVC object is just a shortcut 
+//for creating Region objects. We supply a jQuery selector for each region to manage (e.g., #header, #main, and #footer) and then tell the region to show various 
+//Backbone views within that region. Once the application object has been initialized, we call Backbone.history.start() to route the initial URL.
